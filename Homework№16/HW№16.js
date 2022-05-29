2394b345ce76b5ea0e41ccdec41c39208bdb62bb
Olhae <gerasimchuk.olga93@gmail.com>
2022-05-29 22:31:27 +0300

    Homework№16

---
+++ Homework№16/HW№16.js
@@ -0,0 +1,64 @@
+"https://api.github.com/users/";
+const inpE = document.getElementById("inp-login");
+const containerE = document.querySelector(".container");
+const btnE = document.getElementById("btn");
+const errE = document.querySelector(".error-container");
+let user = null;
+
+const git = new GitHab();
+
+inpE.addEventListener("keyup", validateTodo);
+btnE.addEventListener("click", onFind);
+const ERROR_MSG = {
+  404: "Not found",
+  500: "Server is unavailable",
+};
+
+function onFind(e) {
+  git
+    .getUser(inpE.value)
+    .then((u) => {
+      console.log(u);
+
+      user = u;
+      console.log(u);
+      renderDate(user);
+    })
+    .catch((e) => {
+      renderError(e);
+    });
+  clearValue(inpE);
+}
+
+function clearValue(inpE) {
+  inpE.value = "";
+}
+
+function validateTodo(event) {
+  if (!event.target.value.trim()) {
+    errE.innerText = "";
+    containerE.innerText = "";
+    btnE.disabled = true;
+    return;
+  }
+  if (event.keyCode === 13) {
+    onFind();
+  }
+
+  deleteAll(e);
+}
+
+function deleteAll(event) {
+  if (event.shiftKey) {
+    inpE.value = "";
+  }
+}
+
+function renderDate(date) {
+  containerE.innerHTML = `<li>${date.avatar_url}</li> <li>${date.public_repos}</li> <li>${date.followers}</li> <li>${date.following}</li>`;
+}
+function renderError(errorCode) {
+  errE.innerHTML = ERROR_MSG[errorCode];
+}
+
+// bardankl
---
+++ Homework№16/git/git.js
@@ -0,0 +1,12 @@
+class GitHab {
+  #endpoint = "https://api.github.com/users/";
+
+  getUser(login) {
+    return fetch(this.#endpoint + login).then((r) => {
+      if (r.status === 200) {
+        return r.json();
+      }
+      return Promise.reject(r.status);
+    });
+  }
+}
